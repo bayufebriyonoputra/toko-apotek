b156ed3a4027ee97598ad238fd657923bdbda4f0
Bayu Febriyono Putra <92196679+BayuFebriyono@users.noreply.github.com>
2025-01-09 17:49:58 +0700

    implement dokter royalty

--- app/Filament/Resources/PenjualanResource/Pages/CreatePenjualan.php
+++ app/Filament/Resources/PenjualanResource/Pages/CreatePenjualan.php
@@ -7,6 +7,8 @@ use Filament\Actions;
 use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\CreateRecord;
 use App\Filament\Resources\PenjualanResource\PenjualanResource;
+use App\Models\Dokter;
+use App\Models\Penjualan;
 
 class CreatePenjualan extends CreateRecord
 {
@@ -16,6 +18,19 @@ class CreatePenjualan extends CreateRecord
     {
         $data['user_id'] = Auth::user()->id;
 
+
         return $data;
+    }
+
+    protected function handleRecordCreation(array $data): Penjualan
+    {
+        if ($data['dokter_id']) {
+            $komisi = (int) $data['total'] * 0.005;
+
+            $dokter = Dokter::find($data['dokter_id']);
+            $dokter->royalti_dokter += $komisi;
+            $dokter->save();
+        }
+        return static::getModel()::create($data);
     }
 }
